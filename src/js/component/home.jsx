4616674383f15b6b2eac6acb8b08f19179095a19
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-08-25 18:36:35 +0000

    toggle button added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,10 +4,19 @@ import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 const Home = () => {
-	const [color, setColor] = useState("");
+    const [color, setColor] = useState("");
+    const [index, setIndex] = useState(0);
+    const colors = ["red", "yellow", "green","purple"];
+    const [lights, setLights] = useState([
+        { name: "stoplight-color-red", value: "red" },
+        { name: "stoplight-color-yellow", value: "yellow" },
+        { name: "stoplight-color-green", value: "green" }
+    ]);
 
-	const [lights, setLights] = useState([{ name: "stoplight-color-red", value: "red" }, { name: "stoplight-color-yellow", value: "yellow" }, { name: "stoplight-color-green", value: "green" }])
-
+    const toggleColor = () => {
+        setIndex((prevIndex) => (prevIndex + 1) % 4);  
+        setColor(colors[(index + 1) % 4]); 
+    };
 
 	return (
 		<main id="main-container">
@@ -18,8 +27,8 @@ const Home = () => {
 					console.log(" ya tienes la luz purpura agregada!");
 					
 				}
-				
-			}}>Agregar</button>
+			}}>Agregar Color</button>
+			<button id="toggle-color-button" onClick={toggleColor}>Alternar Color</button>
 			<div id="stoplight-container">
 				<div id="stoplight--top">
 					{

f0f7a09e8ed1d645bc1d69f772d720b970e5b59b
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-08-25 18:21:39 +0000

    traffic light Complete!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
+// include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+
+	const [lights, setLights] = useState([{ name: "stoplight-color-red", value: "red" }, { name: "stoplight-color-yellow", value: "yellow" }, { name: "stoplight-color-green", value: "green" }])
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<main id="main-container">
+			<button id="add-light-button" onClick={()=>{
+				if(lights.length <= 3){
+					setLights([...lights, {name: "stoplight-color-purple", value: "purple"}])
+				}else {
+					console.log(" ya tienes la luz purpura agregada!");
+					
+				}
+				
+			}}>Agregar</button>
+			<div id="stoplight-container">
+				<div id="stoplight--top">
+					{
+						lights.map(lightPerlight => <button className={"" + (color === lightPerlight.value ? lightPerlight.name : '')} onClick={() => setColor(lightPerlight.value)}>
+							<div className="stoplight-color-shadow"></div>
+						</button>)
+					}
+				</div>
+				<div id="stoplight--body"></div>
+				<div id="stoplight--bottom"></div>
+			</div>
+		</main>
 	);
 };
 
